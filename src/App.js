6a01c213dcd1793364d423b2a9483a33837614bb
Taufik Firdaus <t3b094@gmail.com>
2024-03-14 14:30:18 +0000

    deploy

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+           <code>TF</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://LINKTR.EE/TAUFIKFIRDAUS.SSH"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FOLLOW ME
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
